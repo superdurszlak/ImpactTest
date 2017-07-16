bc2e2b0eca9df8794e6f89a134724f6df7753e87
Szymon Durak <superdurszlak@gmail.com>
2017-07-16 23:39:56 +0200

    Composite armor from multiple parts plate generation - instead of composite layup. Working on assemblying plate.

--- ImpactTestKernel.py
+++ ImpactTestKernel.py
@@ -11,11 +11,11 @@ class ImpactTestKernel():
         self.targetRadius = config['armor']['radius']
         self.targetLayers = config['armor']['layers']
         self.meshElementSize = config['meshElementSize']
+        self.assemblyOrder = []
 
     def run(self):
-        self.createTargetPart()
         self.setModelConstants()
-        self.createCompositeLayup()
+        self.createTargetParts()
         self.createModelAssembly()
         self.createProjectileMesh()
         self.createTargetMesh()
@@ -32,18 +32,39 @@ class ImpactTestKernel():
     def setModelConstants(self):
         pass
 
-    def createTargetPart(self):
+    def createTargetParts(self):
         self.__createTargetSketch()
-        part = mdb.models['Model-1'].Part('Target', dimensionality=THREE_D, type=DEFORMABLE_BODY)
-        part.BaseSolidExtrude(mdb.models['Model-1'].sketches['Target-Sketch'], self.__calculateTargetThickness())
-        mdb.models['Model-1'].parts['Target'].DatumCsysByDefault(CARTESIAN)
-        mdb.models['Model-1'].parts['Target'].MaterialOrientation(
-            stackDirection=STACK_3,
-            localCsys=mdb.models['Model-1'].parts['Target'].datums.values()[0]
-        )
+        i = 1
+        for layer in self.targetLayers:
+            name = 'Target-L'+str(i).zfill(3)
+            part = mdb.models['Model-1'].Part(name, dimensionality=THREE_D, type=DEFORMABLE_BODY)
+            part.BaseSolidExtrude(mdb.models['Model-1'].sketches['Target-Sketch'], layer['thickness'])
+            part.DatumCsysByDefault(CARTESIAN)
+            part.ReferencePoint(point=part.InterestingPoint(edge=part.edges[0], rule=CENTER))
+            section = mdb.models['Model-1'].HomogeneousSolidSection(name, str(layer['material']))
+            part.SectionAssignment(
+                sectionName=name,
+                region=regionToolset.Region(
+                    cells=part.cells.getSequenceFromMask(
+                        mask=('[#1 ]', ),
+                    )
+                )
+            )
+            i += 1
+            self.assemblyOrder.append((name, layer['thickness']))
+
 
     def createModelAssembly(self):
-        pass
+        assembly = mdb.models['Model-1'].rootAssembly
+        assembly.DatumCsysByDefault(CARTESIAN)
+        offset = 0.0
+        for element in self.assemblyOrder:
+            name = element[0]
+            thickness = element[1]
+            offset -= thickness
+            part = mdb.models['Model-1'].parts[name]
+            instance = assembly.Instance(name=name, part=part, dependent=ON)
+            assembly.translate(instanceList=(name, ), vector=(0.0, 0.0, offset))
 
     def createJob(self):
         pass
@@ -78,22 +99,6 @@ class ImpactTestKernel():
     def createProjectileMesh(self):
         pass
 
-    def createCompositeLayup(self):
-        part=mdb.models['Model-1'].parts['Target']
-        layup = part.CompositeLayup('Layup', elementType=SOLID)
-        layup.orientation.setValues(
-            orientationType=SYSTEM,
-            localCsys=part.datums[2],
-            stackDirection=STACK_2
-        )
-        cells = part.cells
-        cells = cells.getSequenceFromMask(mask=('[#1 ]', ), )
-        region = regionToolset.Region(cells=cells)
-        i=0
-        for layer in self.targetLayers:
-            self.__plyFor(layup, region, layer, i)
-            i += 1
-
     def __createTargetSketch(self):
         sketch = mdb.models['Model-1'].ConstrainedSketch('Target-Sketch', self.targetRadius*2.0)
         sketch.CircleByCenterPerimeter(center=(0.0, 0.0), point1=(0.0, self.targetRadius))
@@ -103,21 +108,3 @@ class ImpactTestKernel():
         for layer in self.targetLayers:
             thickness += layer['thickness']
         return thickness
-
-    def __plyFor(self, layup, region, layer, idx):
-        material = layer['material']
-        thickness = layer['thickness']
-        layup.CompositePly(
-            suppressed=False,
-            plyName='Ply-'+str(idx)+'-'+str(material),
-            region=region,
-            material=str(material),
-            thicknessType=SPECIFY_THICKNESS,
-            thickness=thickness,
-            orientationType=ANGLE_0,
-            additionalRotationType=ROTATION_NONE,
-            additionalRotationField='',
-            axis=AXIS_3,
-            angle=0.0,
-            numIntPoints=1
-        )
